64691792206ba2bfe18d8fd185cdaa786a31059e
liuqingfeng <504772813@qq.com>
2020-05-28 01:14:13 +0800

    补充,袋装技术说明。继续缺少提升技术

--- r/数据挖掘/交叉树/袋装技术.R
+++ r/数据挖掘/交叉树/袋装技术.R
@@ -19,9 +19,15 @@ set.seed(12345)
 
 #利用bagging建立组合分类树
 #进行25次重抽样自举,生成25棵树。
-#运行结果袋外观测的测试误差为0.447
-
+#运行结果袋外观测的测试误差为0.447。
+#ipred包的bagging函数。
+#bagging(输出变量名~输出变量名,data=数据框,nbagg=k,coob=TRUE,control=参数对象名)
+#coob=TRUE表示基于袋外观测误差。
+#control用于指定袋装过程所建模型的参数。此函数内嵌模型为基础学习器为分类树
+#则control为rpart函数的参数
+#nbagg用于指定自举次数k,默认为25，即25次自举生成25棵分类回归树。
 (BagM1<-bagging(MAILSHOT~.,data=MailShot,nbagg=25,coob=TRUE,control=Ctl))
+#bagging函数执行的结果，但看会打印出预测误差是否能拿到存放在变量里？
 #利用组合分类树对全部观测值进行预测。
 CFit2<-predict(BagM1,MailShot,type="class")
 #计算混淆矩阵
@@ -38,9 +44,23 @@ MailShot<-read.table(file="D:\\书籍资料整理\\《R语言数据挖掘(第2�
 MailShot<-MailShot[,-1]
 Ctl<-rpart.control(minsplit=20,maxcompete=4,maxdepth=30,cp=0.01,xval=10)
 set.seed(12345)
+#adabag包的bagging
+#adabag包中的bagging
+#bagging(输出变量名~输出变量名,data=数据框,mfinal=重复次数,contral=参数对象名)
+#与前边的函数不同的是bagging函数的基础学习器是分类树。
+#mfinal为自举重复次数默认100.
+#bagging函数的基础学习器为分类树,control为rpart函数的参数。
+#此函数返回的是列表。
+#trees中存储k棵分类树的结果。votes存储k个模型的投票情况。
+#prob存储预测类别的概率值。class中存储预测类别。
+#importance中存储输入变量对出书变量预测重要性得分
 BagM2<-bagging(MAILSHOT~.,data=MailShot,control=Ctl,mfinal = 25)
 #这里展示变量的权重
 BagM2$importance
+
+#adabag包中的bagging函数需要用预测函数，predict.bagging
+#predict.bagging返回值与bagging函数返回值相似,只是多了confusion为混淆矩阵
+#error为错判率
 CFit3<-predict.bagging(BagM2,MailShot)
 #混淆矩阵
 CFit3$confusion
