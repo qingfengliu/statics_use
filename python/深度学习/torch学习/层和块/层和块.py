7e73d546bbf6dd92ce4cd93b19fea2bf37bfc15d
liuqingfeng <504772813@qq.com>
2022-02-07 10:07:37 +0800

    年前忘记存储

---
+++ python/深度学习/torch学习/层和块/层和块.py
@@ -0,0 +1,47 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+X = torch.rand(2, 20)
+
+#自定义块
+class MLP(nn.Module):
+    # 用模型参数声明层。这里，我们声明两个全连接的层
+    def __init__(self):
+        # 调用MLP的父类Module的构造函数来执行必要的初始化。
+        # 这样，在类实例化时也可以指定其他函数参数，例如模型参数params（稍后将介绍）
+        super().__init__()
+        self.hidden = nn.Linear(20, 256)  # 隐藏层
+        self.out = nn.Linear(256, 10)  # 输出层
+
+    # 定义模型的前向传播，即如何根据输入X返回所需的模型输出
+    def forward(self, X):
+        # 注意，这里我们使用ReLU的函数版本，其在nn.functional模块中定义。
+        return self.out(F.relu(self.hidden(X)))
+
+net = MLP()
+print(net(X))
+
+#除非我们实现一个新的运算符，
+# 否则我们不必担心反向传播函数或参数初始化，
+# 系统将自动生成这些。
+
+
+#顺序块,按照参数传入顺序执行(同nn.Sequential)
+class MySequential(nn.Module):
+    def __init__(self, *args):
+        super().__init__()
+        for idx, module in enumerate(args):
+            # 这里，module是Module子类的一个实例。我们把它保存在'Module'类的成员
+            # 变量_modules中。module的类型是OrderedDict
+            self._modules[str(idx)] = module
+
+    def forward(self, X):
+        # OrderedDict保证了按照成员添加的顺序遍历它们
+        for block in self._modules.values():
+            X = block(X)
+        return X
+
+net = MySequential(nn.Linear(20, 256), nn.ReLU(), nn.Linear(256, 10))
+print(net(X))
